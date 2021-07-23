269a413e03d12b04845d4f9b61d4de71b553c762
Sava <71556255+YashaBlendeer@users.noreply.github.com>
2021-07-23 17:57:26 +0300

    Task Stream

---
+++ Task 6 Stream/src/TaskStream.java
@@ -0,0 +1,32 @@
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class TaskStream {
+    public static void main(String[] args) {
+        int[] nums = {4, 6, 1, 78, 0, 43, 2, 17, -15, 0};
+
+        System.out.println(calcAverage(nums));
+        System.out.println(getZeroElements(nums));
+        System.out.println(getPositiveElements(nums));
+        multiplyElements(nums, 2);
+
+    }
+
+    public static double calcAverage(int[] arr) {
+        return IntStream.of(arr).filter(x -> x != 0).average().getAsDouble();
+    }
+
+    public static int getZeroElements(int[] arr) {
+        return (int) IntStream.of(arr).filter(x -> x == 0).count();
+    }
+
+    public static int getPositiveElements(int[] arr) {
+        return (int) IntStream.of(arr).filter(x -> x > 0).count();
+    }
+
+    public static void multiplyElements(int[] arr, int factor) {
+        IntStream.of(arr).map(x -> x * factor).forEach(x -> System.out.format("%s, ", x));
+    }
+
+
+}
