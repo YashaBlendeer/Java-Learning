98c5293926ef6195e9b876cb0a48a44efb239f11
Sava <71556255+YashaBlendeer@users.noreply.github.com>
2021-07-24 13:11:10 +0300

    Update Task Stream

--- Task 6 Stream/src/TaskStream.java
+++ Task 6 Stream/src/TaskStream.java
@@ -1,19 +1,35 @@
-import java.util.Comparator;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TaskStream {
     public static void main(String[] args) {
-        int[] nums = {4, 6, 1, 78, 0, 43, 2, 17, -15, 0};
-
+        int[] nums = {4, 6, 1, 17, -15, 0};
         System.out.println(calcAverage(nums));
         System.out.println(getZeroElements(nums));
         System.out.println(getPositiveElements(nums));
         multiplyElements(nums, 2);
-
+        System.out.println(findMinElement(nums));
     }
 
     public static double calcAverage(int[] arr) {
         return IntStream.of(arr).filter(x -> x != 0).average().getAsDouble();
+    }
+
+    public static Map.Entry<Integer, Integer> findMinElement(int[] arr) {
+        TreeMap<Integer, Integer> map = IntStream
+                .range(0, arr.length) //0 1 2 3 4 5
+                .boxed()
+                .collect(Collectors.toMap(
+                        i -> arr[i],
+                        Function.identity(),
+                        (oldValue,
+                         newValue)
+                                -> newValue,
+                        TreeMap::new
+                ));
+        return map.firstEntry();
     }
 
     public static int getZeroElements(int[] arr) {
